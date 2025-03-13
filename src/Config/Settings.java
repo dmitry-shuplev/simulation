4f20856164e7fc683587cedd1e4e6fe419d673c7
j_dim <dmitrijsuplev@gmail.com>
2025-03-13 09:57:09 +0300

    create_enum_with_entites

--- src/Config/Settings.java
+++ src/Config/Settings.java
@@ -5,12 +5,33 @@ public class Settings {
     public static final int Y_MAX = 10;
     public static final int STEPS = 10;
 
-    public static final int TOTAL_HERBVORE = 0;
-
-    public enum Direction{
+    public enum Direction {
         UP,
         DOWN,
         RIGHT,
         LEFT
     }
+
+    public enum Entites {
+        GRASS(1, "Grass"),
+        HERBVORE(2, "Herbvore");
+        private final int totalObjects;
+        private final String entityName;
+
+        Entites(int totalObjects, String entityName){
+            this.totalObjects = totalObjects;
+            this.entityName = entityName;
+        }
+
+        public int getTotalObjects(){
+            return this.totalObjects;
+        }
+
+        public String getEntityName() {
+            return entityName;
+        }
+    }
+
+
+//end of enum
 }
