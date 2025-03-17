4466719f089a3eeff4221bdfdb6b6ebaf3c70126
j_dim <dmitrijsuplev@gmail.com>
2025-03-17 16:57:34 +0300

    start_creation_methods_for_view_with_swing

---
+++ src/MapComponents.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+import Map.*;
+
+public class MapComponents(Map map) extends JComponent {
+
+}
--- src/View.java
+++ src/View.java
@@ -2,16 +2,18 @@ import Map.*;
 import Config.Settings.*;
 import Subjects.Entity;
 
+import javax.swing.*;
+import java.awt.*;
 import java.lang.ref.Cleaner;
 
-import static Config.Settings.X_MAX;
-import static Config.Settings.Y_MAX;
+import static Config.Settings.MAP_LENGTH;
+import static Config.Settings.MAP_HEIGHT;
 
 public class View {
 
     public static void showMap(Map map) {
-        for (int y = Y_MAX-1; y >= 0; y--) {
-            for (int x = 0; x <= X_MAX-1; x++) {
+        for (int y = MAP_HEIGHT-1; y >= 0; y--) {
+            for (int x = 0; x <= MAP_LENGTH-1; x++) {
                 Coordinate coordinate = new Coordinate(x, y);
                 if (map.getMap().get(coordinate) == null) {
                     System.out.print(" * ");
@@ -35,12 +37,48 @@ public class View {
 
     public static void clearConsole() {
         try {
-            // Команда для очистки терминала в Windows
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         } catch (Exception e) {
             System.out.println("Ошибка при очистке терминала: " + e.getMessage());
         }
     }
+
+    public static void showMapSwing(Map map){
+        JFrame frame = new JFrame();
+        frame.setSize(1200, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //GraphModel graphModel = new GraphModel();
+
+        BorderLayout borderLayout = new BorderLayout();
+       // FieldComponent field = new FieldComponent(graphModel);
+        Button buttonStartWide = new Button("Сгенерировать карту");
+        Button buttonStartDeep = new Button("Сделать ход");
+        Button buttonReset = new Button("Запустить карту");
+        Button buttonStepWide = new Button("Шаг в ширину");
+        Button buttonStepDeep = new Button("Шаг в глубину");
+
+        JPanel buttons = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+        buttons.add(buttonStartWide);
+        buttons.add(buttonStartDeep);
+        buttons.add(buttonStepWide);
+        buttons.add(buttonStepDeep);
+        buttons.add(buttonReset);
+
+        frame.getContentPane().setLayout(borderLayout);
+        //frame.getContentPane().add(field, BorderLayout.CENTER);
+        frame.getContentPane().add(buttons, BorderLayout.PAGE_END);
+
+        /*buttonReset.addActionListener(e -> field.clear());
+        buttonStartWide.addActionListener(e -> field.startWide());
+        buttonStartDeep.addActionListener(e -> field.startDeep());
+        buttonStepWide.addActionListener(e -> field.stepWide());
+        buttonStepDeep.addActionListener(e -> field.stepDeep());*/
+
+        frame.setVisible(true);
+       // field.clear();
+    }
+
 
 
 }
