af259567a055d6d3e295c81d7e38afb4159c48b7
j_dim <dmitrijsuplev@gmail.com>
2025-03-18 12:30:06 +0300

    work_version_show_map

--- src/MapComponents.java
+++ src/MapComponents.java
@@ -7,24 +7,26 @@ import Map.*;
 import java.awt.*;
 
 public class MapComponents extends JComponent {
-    Map currentMap;
-    public MapComponents(Map map){
+    private Map currentMap;
+
+    public MapComponents(Map map) {
         this.currentMap = map;
     }
+
     @Override
     public void paint(Graphics g) {
         //super.paint(g);
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, Settings.MAP_WIDTH, Settings.MAP_HEIGHT);
 
-        int cellSize = getWidth()/Settings.MAP_WIDTH;
+        int cellSize = getWidth() / Settings.MAP_WIDTH;
 
         for (int y = 0; y < Settings.MAP_HEIGHT; y++) {
             for (int x = 0; x < Settings.MAP_WIDTH; x++) {
                 switch (getEntitySymbol(x, y)) {
                     case 'P':
                         g.setColor(Color.RED);
-                        break;
+                          break;
                     case 'H':
                         g.setColor(Color.PINK);
                         break;
@@ -40,7 +42,6 @@ public class MapComponents extends JComponent {
                     default:
                         g.setColor(Color.LIGHT_GRAY);
                 }
-
                 g.fillRect(x * cellSize, y * cellSize, (int) (cellSize / 1.2), (int) (cellSize / 1.2));
             }
         }
@@ -48,8 +49,9 @@ public class MapComponents extends JComponent {
 
     private char getEntitySymbol(int x, int y) {
         Coordinate coordinate = new Coordinate(x, y);
-if(currentMap.getMap().get(coordinate)!=null){
-    return currentMap.getMap().get(coordinate).getView();}
+        if (currentMap.getMap().get(coordinate) != null) {
+            return currentMap.getMap().get(coordinate).getView();
+        }
         return 'E';
     }
 
