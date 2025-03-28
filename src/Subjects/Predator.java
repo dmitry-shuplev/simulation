0abd02b619ccea5af85669857c7cc0d4a662642b
”Dmitry <dmitrijsuplev@gmail.com>
2025-03-28 08:23:47 +0300

    debug_mistake_with_doble_constructor_rock_and_predator

--- src/Subjects/Predator.java
+++ src/Subjects/Predator.java
@@ -3,14 +3,11 @@ package Subjects;
 import Map.Coordinate;
 
 public class Predator extends Creature{
-    public Predator(Coordinate coordinate) {
-        setCoordinate(coordinate);}
 
     private final char representSymbol = 'P';
 
     public Predator(Coordinate coordinate) {
-        setCoordinate(coordinate);
-    }
+        setCoordinate(coordinate);}
 
     public char getView() {
         return representSymbol;
--- src/Subjects/Rock.java
+++ src/Subjects/Rock.java
@@ -3,8 +3,6 @@ package Subjects;
 import Map.Coordinate;
 
 public class Rock extends Entity {
-    public Rock(Coordinate coordinate) {
-        setCoordinate(coordinate);
 
     private final char representSymbol = 'R';
 
